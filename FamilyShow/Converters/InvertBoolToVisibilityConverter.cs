850645f2199a5b42229016fbcb98c626ae55ad53
iivxsiuol <personal@personal.fr>
2020-08-04 20:52:07 +0200

    iivxsiuol: do not show date and place of death when IsLiving !

---
+++ FamilyShow/Converters/InvertBoolToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Microsoft.FamilyShow.Converters
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class InvertBoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var conv = new BoolToVisibilityConverter();
+            return conv.Convert(!(bool)value, targetType, parameter, culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var conv = new BoolToVisibilityConverter();
+            return !(bool)conv.ConvertBack(value, targetType, parameter, culture);
+        }
+    }
+}
